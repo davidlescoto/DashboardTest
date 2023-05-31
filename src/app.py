7c5ac48d7f2417a6d0c17bf77010a05a47ccd50e
davidescoto <davidlescoto@gmail.com>
2023-05-31 16:48:34 -0600

    antes de deploy la app

--- src/app.py
+++ src/app.py
@@ -8,6 +8,7 @@ from dash import html
 #external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, pages_folder = "../pages",use_pages=True, external_stylesheets=[dbc.themes.YETI])
+server = app.server
 #app._favicon = ("../favicon_urc.ico")
 header = dbc.Navbar(
     dbc.Container(
